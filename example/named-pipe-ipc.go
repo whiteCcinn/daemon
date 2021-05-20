ba79972e4816020c094b369716e194e6f6e61524
whiteCcinn <471113744@qq.com>
2021-05-20 18:35:48 +0800

    support named-pipe-ipc for external process

---
+++ example/named-pipe-ipc.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/whiteCcinn/daemon"
+	named_pipe_ipc "github.com/whiteCcinn/named-pipe-ipc"
+	"log"
+)
+
+func main() {
+	nctx, err := named_pipe_ipc.NewContext(context.Background(), "./", named_pipe_ipc.C)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message := daemon.NamedPipeMessage{
+		Api: daemon.PrintInformation,
+	}
+
+	data, err := json.Marshal(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, _ = nctx.Send(named_pipe_ipc.Message(string(data) + "\n"))
+	msg, err := nctx.Recv(true, '\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(msg)
+}
